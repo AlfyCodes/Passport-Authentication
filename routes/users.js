c9841819e7ca01a7a7a19c2363a56e31b1a6bdf8
AlfyCodes <alfred.ortiz1@marist.edu>
2020-10-04 00:01:04 -0400

    Connection to MongooseDB Atlas + able to submit new user

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,12 @@ router.get('/login', (req, res) => res.render('login'));
 // Register Page
 router.get('/register', (req, res) => res.render('register'));
 
+// Register Handle
+
+router.post('/register', (req, res) =>{
+
+    console.log(req.body)
+    res.send('hello')
+})
+
 module.exports = router;
